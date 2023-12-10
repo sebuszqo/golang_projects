20e79f380552ac03709f50cef02d1fdc6d7bec69
sknap <sknap@splunk.com>
2023-12-10 18:34:16 +0100

    Add formHandler

--- simple-web-server/main.go
+++ simple-web-server/main.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"strings"
 	"time"
 )
 
@@ -11,6 +12,46 @@ const (
 	getMethod  = "GET"
 	postMethod = "POST"
 )
+
+func formHandler(w http.ResponseWriter, r *http.Request) {
+	if r.Method != postMethod {
+		http.Error(w, "method is not supported", http.StatusNotFound)
+		return
+	}
+
+	if err := r.ParseForm(); err != nil {
+		http.Error(w, fmt.Sprintf("ParseForm() err: %v", err), http.StatusInternalServerError)
+		return
+	}
+
+	w.Write([]byte("POST request successful\n"))
+
+	name := r.PostForm.Get("name")
+	address := r.PostForm.Get("address")
+
+	if name == "" || address == "" {
+		http.Error(w, "Name and Address are required fields", http.StatusBadRequest)
+		return
+	}
+
+	const maxNameLength = 50
+	const maxAddressLength = 100
+
+	if len(name) > maxNameLength || len(address) > maxAddressLength {
+		http.Error(w, "Name or Address exceeds the maximum allowed length", http.StatusBadRequest)
+		return
+	}
+
+	w.Write([]byte(fmt.Sprintf("Hello: %s, from %s", capitalizeFirstLetter(name), capitalizeFirstLetter(address))))
+}
+
+func capitalizeFirstLetter(input string) string {
+	if input == "" {
+		return input
+	}
+
+	return strings.ToUpper(string(input[0])) + input[1:]
+}
 
 func helloHandler(w http.ResponseWriter, r *http.Request) {
 	if r.Method != getMethod {
