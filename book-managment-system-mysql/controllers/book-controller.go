1b55fa63f64045dc292d83935ea4d6d6ebc035cf
sebuszqo <sknap@splunk.com>
2023-12-15 20:28:57 +0100

    DeleteBook Controller has been added

--- book-managment-system-mysql/controllers/book-controller.go
+++ book-managment-system-mysql/controllers/book-controller.go
@@ -64,7 +64,29 @@ func GetBookById(w http.ResponseWriter, r *http.Request) {
 func UpdateBook(w http.ResponseWriter, r *http.Request) {
 
 }
+
 func DeleteBook(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodDelete {
+		w.WriteHeader(http.StatusMethodNotAllowed)
+		return
+	}
+	vars := mux.Vars(r)
+	bookId := vars["bookId"]
+	ID, err := strconv.ParseInt(bookId, 0, 0)
+	if err != nil {
+		handleError(w, http.StatusInternalServerError, "Internal Server Error")
+		return
+	}
+	newBook := models.DeleteBook(ID)
+	res, err := json.Marshal(newBook)
+	if err != nil {
+		handleError(w, http.StatusInternalServerError, "Internal Server Error")
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	w.Write(res)
 
 }
 
