5e91537b6b895b23e263018ed11bd3a34df56e84
sebuszqo <sknap@splunk.com>
2023-12-15 22:06:17 +0100

    Made code easier to read and easier to understand + added returning errors

--- book-managment-system-mysql/utils/utils.go
+++ book-managment-system-mysql/utils/utils.go
@@ -7,10 +7,15 @@ import (
 	"net/http"
 )
 
-func ParseBody(r *http.Request, X interface{}) {
-	if body, err := io.ReadAll(r.Body); err == nil {
-		if err := json.Unmarshal(body, X); err != nil {
-			return
-		}
+func ParseBody(r *http.Request, X interface{}) error {
+	body, err := io.ReadAll(r.Body)
+	if err != nil {
+		return err
 	}
+
+	if err := json.Unmarshal(body, X); err != nil {
+		return err
+	}
+
+	return nil
 }
