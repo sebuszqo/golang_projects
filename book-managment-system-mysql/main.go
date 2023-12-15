ad079a23ed4ed91595f6ce46232303a896d8dd6f
sebuszqo <sknap@splunk.com>
2023-12-15 19:54:07 +0100

    main and parser added

---
+++ book-managment-system-mysql/main.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"book-managment-system-mysql/routes"
+	"github.com/gorilla/mux"
+	"log"
+	"net/http"
+)
+
+func main() {
+	r := mux.NewRouter()
+	routes.RegisterBookStoreRoutes(r)
+	http.Handle("/", r)
+	log.Fatal(http.ListenAndServe("localhost:8080", r))
+}
