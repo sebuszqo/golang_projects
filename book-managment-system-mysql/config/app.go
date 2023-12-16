efda68cdb0750078f24431d6219d34dc9137b7cb
sebuszqo <sknap@splunk.com>
2023-12-16 13:31:13 +0100

    changed sql settings

--- book-managment-system-mysql/config/app.go
+++ book-managment-system-mysql/config/app.go
@@ -1,13 +1,16 @@
 package config
 
-import "github.com/jinzhu/gorm"
+import (
+	"github.com/jinzhu/gorm"
+	_ "github.com/jinzhu/gorm/dialects/mysql"
+)
 
 var (
 	db *gorm.DB
 )
 
 func Connect() {
-	d, err := gorm.Open("mysql", "akhil:Axlesharma@12@/simplerest?charset=utf8&parseTime=True&loc=Local")
+	d, err := gorm.Open("mysql", "sebuszqo:sebuszqo@/simplerest?charset=utf8&parseTime=True&loc=Local")
 	if err != nil {
 		panic(err)
 	}
